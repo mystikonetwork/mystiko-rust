2254d7f4e6f5ef0bf44a8aa6756c7c23e742c474
ericlurker <96100006+ericlurker@users.noreply.github.com>
2023-02-28 14:20:47 +0800

    coverage ignore mystiko_abi

--- mystiko_abi/src/lib.rs
+++ mystiko_abi/src/lib.rs
@@ -2,17 +2,15 @@
 
 extern crate serde;
 
-pub mod zk_rollup;
-pub mod zk_transact;
-
+#[ignore]
 pub mod commitment_pool;
 pub mod commitment_pool_erc20;
 pub mod commitment_pool_main;
-
+pub mod mystiko_v2_bridge;
 pub mod mystiko_v2_loop;
 pub mod mystiko_v2_loop_erc20;
 pub mod mystiko_v2_loop_main;
-
-pub mod mystiko_v2_bridge;
 pub mod mystiko_v2t_bridge_erc20;
 pub mod mystiko_v2t_bridge_main;
+pub mod zk_rollup;
+pub mod zk_transact;
