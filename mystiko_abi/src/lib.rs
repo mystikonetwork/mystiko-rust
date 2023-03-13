dcacbe0e1c8d0b0671994d9e8ad122b5b87b14ed
ericlurker <96100006+ericlurker@users.noreply.github.com>
2023-03-13 11:16:40 +0800

    add erc20 abi

--- mystiko_abi/src/lib.rs
+++ mystiko_abi/src/lib.rs
@@ -5,6 +5,7 @@ extern crate serde;
 pub mod commitment_pool;
 pub mod commitment_pool_erc20;
 pub mod commitment_pool_main;
+pub mod erc20;
 pub mod mystiko_v2_bridge;
 pub mod mystiko_v2_loop;
 pub mod mystiko_v2_loop_erc20;
