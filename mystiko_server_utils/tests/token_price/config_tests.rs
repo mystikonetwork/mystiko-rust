c16b1603e756a9cbd1878274f84cda759f7d1b41
ericlurker <96100006+ericlurker@users.noreply.github.com>
2023-05-24 17:01:20 +0800

    remove to in TxBuilder

--- mystiko_server_utils/tests/token_price/config_tests.rs
+++ mystiko_server_utils/tests/token_price/config_tests.rs
@@ -3,19 +3,19 @@ use std::env;
 
 #[tokio::test]
 async fn test_read_config() {
-    let cfg = TokenPriceConfig::new("testnet", "").unwrap();
+    let cfg = TokenPriceConfig::new("testnet", None).unwrap();
     assert_eq!(cfg.price_cache_ttl, 72000);
 
-    let cfg = TokenPriceConfig::new("mainnet", "").unwrap();
+    let cfg = TokenPriceConfig::new("mainnet", None).unwrap();
     assert_eq!(cfg.price_cache_ttl, 1800);
 
-    let cfg = TokenPriceConfig::new("mainnet", "tests/token_price/files/config").unwrap();
+    let cfg = TokenPriceConfig::new("mainnet", Some("tests/token_price/files/config")).unwrap();
     assert_eq!(cfg.price_cache_ttl, 90000);
 
     env::set_var("MYSTIKO_TOKEN_PRICE.PRICE_CACHE_TTL", "800");
-    let cfg = TokenPriceConfig::new("mainnet", "").unwrap();
+    let cfg = TokenPriceConfig::new("mainnet", None).unwrap();
     assert_eq!(cfg.price_cache_ttl, 800);
 
-    let cfg = TokenPriceConfig::new("mainnet", "tests/token_price/files/config").unwrap();
+    let cfg = TokenPriceConfig::new("mainnet", Some("tests/token_price/files/config")).unwrap();
     assert_eq!(cfg.price_cache_ttl, 800);
 }
