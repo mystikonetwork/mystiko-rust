78c34390690cf434373b8c34a8d22911e15d7425
moomarco <iamwumaixing@gmail.com>
2023-07-04 19:03:27 +0800

    update relayer server main

--- mystiko_relayer_server/src/application.rs
+++ mystiko_relayer_server/src/application.rs
@@ -27,66 +27,68 @@ pub struct Application {
 pub struct ApplicationOptions<'a> {
     host: &'a str,
     port: u16,
-    array_queue_capacity: u64,
+    array_queue_capacity: usize,
     app_state_options: AppStateOptions<'a>,
 }
 
-#[allow(clippy::needless_lifetimes)]
-pub async fn init_http_server<'a>(options: ApplicationOptions<'a>) -> Result<Application> {
-    // init app state
-    let app_state = init_app_state(options.app_state_options).await?;
+impl Application {
+    #[allow(clippy::needless_lifetimes)]
+    pub async fn new<'a>(options: ApplicationOptions<'a>) -> Result<Self> {
+        // init app state
+        let app_state = init_app_state(options.app_state_options).await?;
 
-    // init sqlite db connection
-    let db = Arc::new(init_sqlite_database(&app_state.server_config.sqlite_db_path).await?);
+        // init sqlite db connection
+        let db = Arc::new(init_sqlite_database(&app_state.server_config.sqlite_db_path).await?);
 
-    // create account handler
-    let account_handler = Arc::new(AccountHandler::new(db.clone(), &app_state.server_config.accounts).await?);
+        // create account handler
+        let account_handler = Arc::new(AccountHandler::new(db.clone(), &app_state.server_config.accounts).await?);
 
-    // create transaction handler
-    let transaction_handler = Arc::new(TransactionHandler::new(db.clone()));
+        // create transaction handler
+        let transaction_handler = Arc::new(TransactionHandler::new(db.clone()));
 
-    // init token price
-    let token_price = Arc::new(RwLock::new(TokenPrice::new(
-        &TokenPriceConfig::new(
-            serde_json::to_string(&app_state.server_config.network_type)?.as_str(),
-            None,
-        )?,
-        &app_state.server_config.coin_market_cap_api_key,
-    )?));
+        // init token price
+        let token_price = Arc::new(RwLock::new(TokenPrice::new(
+            &TokenPriceConfig::new(
+                serde_json::to_string(&app_state.server_config.network_type)?.as_str(),
+                None,
+            )?,
+            &app_state.server_config.coin_market_cap_api_key,
+        )?));
 
-    // init transact channel
-    let providers = Arc::new(RwLock::new(
-        ProviderPool::builder()
-            .chain_providers_options(Box::new(app_state.clone()))
-            .build(),
-    ));
+        // init transact channel
+        let providers = Arc::new(RwLock::new(
+            ProviderPool::builder()
+                .chain_providers_options(Box::new(app_state.clone()))
+                .build(),
+        ));
 
-    let (senders, consumers) = transact_channel::init(
-        &app_state.server_config,
-        &app_state.relayer_config,
-        providers.clone(),
-        transaction_handler.clone(),
-        token_price.clone(),
-        options.array_queue_capacity as usize,
-    )
-    .await?;
+        let (senders, consumers) = transact_channel::init(
+            &app_state.server_config,
+            &app_state.relayer_config,
+            providers.clone(),
+            transaction_handler.clone(),
+            token_price.clone(),
+            options.array_queue_capacity,
+        )
+        .await?;
 
-    let http_server = HttpServer::new(move || {
-        App::new()
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-            .app_data(Data::new(app_state.clone()))
-            .app_data(Data::new(senders.clone()))
-            .app_data(Data::new(account_handler.clone()))
-            .app_data(Data::new(transaction_handler.clone()))
-            .app_data(Data::new(token_price.clone()))
-            .app_data(Data::new(providers.clone()))
-            .service(info)
-            .service(transact)
-            .service(transaction_status)
-    })
-    .bind((options.host, options.port))?
-    .run();
+        let http_server = HttpServer::new(move || {
+            App::new()
+                .wrap(Logger::default())
+                .wrap(Logger::new("%a %{User-Agent}i"))
+                .app_data(Data::new(app_state.clone()))
+                .app_data(Data::new(senders.clone()))
+                .app_data(Data::new(account_handler.clone()))
+                .app_data(Data::new(transaction_handler.clone()))
+                .app_data(Data::new(token_price.clone()))
+                .app_data(Data::new(providers.clone()))
+                .service(info)
+                .service(transact)
+                .service(transaction_status)
+        })
+        .bind((options.host, options.port))?
+        .run();
 
-    Ok(Application::builder().server(http_server).consumers(consumers).build())
+        Ok(Application::builder().server(http_server).consumers(consumers).build())
+    }
 }
--- mystiko_relayer_server/src/main.rs
+++ mystiko_relayer_server/src/main.rs
@@ -1,19 +1,7 @@
-use actix_web::middleware::Logger;
-use actix_web::web::Data;
-use actix_web::{App, HttpServer};
 use anyhow::Result;
 use log::info;
-use mystiko_ethers::provider::pool::ProviderPool;
-use mystiko_relayer_server::channel::transact_channel;
-use mystiko_relayer_server::common::{init_app_state, AppStateOptions};
-use mystiko_relayer_server::database::init_sqlite_database;
-use mystiko_relayer_server::handler::account::AccountHandler;
-use mystiko_relayer_server::handler::transaction::TransactionHandler;
-use mystiko_relayer_server::service::{info, transact, transaction_status};
-use mystiko_server_utils::token_price::config::TokenPriceConfig;
-use mystiko_server_utils::token_price::price::TokenPrice;
-use std::sync::Arc;
-use tokio::sync::RwLock;
+use mystiko_relayer_server::application::{Application, ApplicationOptions};
+use mystiko_relayer_server::common::AppStateOptions;
 
 pub const DEFAULT_CONFIG_PATH: &str = "./config.toml";
 pub const HOST: &str = "127.0.0.1";
@@ -29,78 +17,33 @@ async fn main() -> Result<()> {
     let relayer_config_path = args.get(3).map(|path| Some(path.as_str())).unwrap_or(None);
     let mystiko_config_path = args.get(4).map(|path| Some(path.as_str())).unwrap_or(None);
 
-    // init app state
-    let app_state = init_app_state(
-        AppStateOptions::builder()
-            .log_level(log_level)
-            .server_config_path(server_config_path)
-            .relayer_config_path(relayer_config_path)
-            .mystiko_config_path(mystiko_config_path)
+    let application = Application::new(
+        ApplicationOptions::builder()
+            .host(HOST)
+            .port(PORT)
+            .app_state_options(
+                AppStateOptions::builder()
+                    .log_level(log_level)
+                    .server_config_path(server_config_path)
+                    .relayer_config_path(relayer_config_path)
+                    .mystiko_config_path(mystiko_config_path)
+                    .build(),
+            )
+            .array_queue_capacity(ARRAY_QUEUE_CAPACITY)
             .build(),
-    )
-    .await?;
-
-    // init sqlite db connection
-    let db = Arc::new(init_sqlite_database(&app_state.server_config.sqlite_db_path).await?);
-
-    // create account handler
-    let account_handler = Arc::new(AccountHandler::new(db.clone(), &app_state.server_config.accounts).await?);
-
-    // create transaction handler
-    let transaction_handler = Arc::new(TransactionHandler::new(db.clone()));
-
-    // init token price
-    let token_price = Arc::new(RwLock::new(TokenPrice::new(
-        &TokenPriceConfig::new(
-            serde_json::to_string(&app_state.server_config.network_type)?.as_str(),
-            None,
-        )?,
-        &app_state.server_config.coin_market_cap_api_key,
-    )?));
-
-    // init transact channel
-    let providers = Arc::new(RwLock::new(
-        ProviderPool::builder()
-            .chain_providers_options(Box::new(app_state.clone()))
-            .build(),
-    ));
-    let (senders, consumers) = transact_channel::init(
-        &app_state.server_config,
-        &app_state.relayer_config,
-        providers.clone(),
-        transaction_handler.clone(),
-        token_price.clone(),
-        ARRAY_QUEUE_CAPACITY,
     )
     .await?;
 
     // run transact consumers
-    for consumer in consumers {
+    for consumer in application.consumers {
         tokio::spawn(async {
             consumer.run().await;
         });
     }
 
-    info!("Server startup [host] {} [port] {}", HOST, PORT);
-
-    // run http server
-    HttpServer::new(move || {
-        App::new()
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-            .app_data(Data::new(app_state.clone()))
-            .app_data(Data::new(senders.clone()))
-            .app_data(Data::new(account_handler.clone()))
-            .app_data(Data::new(transaction_handler.clone()))
-            .app_data(Data::new(token_price.clone()))
-            .app_data(Data::new(providers.clone()))
-            .service(info)
-            .service(transact)
-            .service(transaction_status)
-    })
-    .bind((HOST, PORT))?
-    .run()
-    .await?;
+    // server await
+    info!("Application server start at {}:{}", HOST, PORT);
+    application.server.await?;
 
     Ok(())
 }
