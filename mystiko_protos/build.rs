3ec076f958f33891372275a700c1dfc18e36e2c2
Fitch Li <fitch@mystiko.network>
2023-07-20 18:55:43 +0800

    fix build failure

--- mystiko_protos/build.rs
+++ mystiko_protos/build.rs
@@ -6,15 +6,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let output = Command::new("buf")
         .arg("generate")
         .current_dir(env!("CARGO_MANIFEST_DIR"))
-        .output()
-        .expect("failed to execute process");
-    io::stdout().write_all(&output.stdout)?;
-    io::stderr().write_all(&output.stderr)?;
-    if !output.status.success() {
-        print!(
-            "cargo:warning={}",
-            "Protobuf code generate failed, use previous generated files"
-        );
+        .output();
+    let success = match output {
+        Ok(output) => {
+            io::stdout().write_all(&output.stdout)?;
+            io::stderr().write_all(&output.stderr)?;
+            output.status.success()
+        }
+        Err(e) => {
+            println!("cargo:warning={}", e);
+            false
+        }
+    };
+    if !success {
+        print!("cargo:warning=Protobuf code generate failed, use previous generated files");
     }
     Ok(())
 }
