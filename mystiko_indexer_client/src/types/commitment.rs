96cec9fef52e59e85749dfa666c006dbd38feb8a
3210m <96099839+3210m@users.noreply.github.com>
2023-08-08 18:32:31 +0800

    add interfaces for dataLoader (#94)

--- mystiko_indexer_client/src/types/commitment.rs
+++ mystiko_indexer_client/src/types/commitment.rs
@@ -16,45 +16,45 @@ pub enum DepositStatus {
 #[derive(Deserialize, Serialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct CommitmentFilter {
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub status: Option<DepositStatus>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub chain_id: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub contract_address: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub commit_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub rollup_fee: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub encrypted_note: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub leaf_index: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub block_num: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub src_chain_id: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub src_address: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub creation_transaction_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub relay_transaction_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub rollup_transaction_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub create_at: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub update_at: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub contract_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub src_contract_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub transaction_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub rollup_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub deposit_id: Option<u32>,
 }
 
@@ -62,11 +62,11 @@ pub struct CommitmentFilter {
 pub struct CommitmentsForContractRequest {
     pub chain_id: u64,
     pub contract_address: String,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub start_block: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub end_block: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub where_filter: Option<CommitmentFilter>,
 }
 
@@ -75,38 +75,70 @@ pub struct CommitmentsForContractRequest {
 pub struct CommitmentResponse {
     pub id: u32,
     pub status: DepositStatus,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub chain_id: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub contract_address: Option<String>,
     pub commit_hash: String,
     pub rollup_fee: String,
     pub encrypted_note: String,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub leaf_index: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub block_num: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub src_chain_id: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub src_address: Option<String>,
     pub creation_transaction_hash: String,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub relay_transaction_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub rollup_transaction_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub create_at: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub update_at: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub contract_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub src_contract_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub transaction_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub rollup_id: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub deposit_id: Option<u32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
+pub struct CommitmentForDataLoaderResponse {
+    pub contract_address: String,
+    pub current_sync_block_num: u64,
+    pub commitments: Vec<CommitmentForDataLoader>,
+}
+
+#[derive(Debug, Serialize, Deserialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
+pub struct CommitmentForDataLoader {
+    pub commitment_hash: String,
+    pub status: DepositStatus,
+    pub block_number: u64,
+    #[builder(setter(strip_option), default = None)]
+    pub included_block_number: Option<u64>,
+    #[builder(setter(strip_option), default = None)]
+    pub src_chain_block_number: Option<u64>,
+    #[builder(setter(strip_option), default = None)]
+    pub leaf_index: Option<u64>,
+    #[builder(setter(strip_option), default = None)]
+    pub rollup_fee: Option<String>,
+    #[builder(setter(strip_option), default = None)]
+    pub encrypted_note: Option<String>,
+    #[builder(setter(strip_option), default = None)]
+    pub queued_transaction_hash: Option<String>,
+    #[builder(setter(strip_option), default = None)]
+    pub included_transaction_hash: Option<String>,
+    #[builder(setter(strip_option), default = None)]
+    pub src_chain_transaction_hash: Option<String>,
 }
--- mystiko_indexer_client/src/types/commitment_spent.rs
+++ mystiko_indexer_client/src/types/commitment_spent.rs
@@ -1,39 +1,40 @@
+use crate::types::commitment::CommitmentForDataLoaderResponse;
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
 
 #[derive(Deserialize, Serialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
 pub struct CommitmentSpentFilter {
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub chain_id: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub contract_address: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub root_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub tx_hash: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub serial_num: Option<String>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub block_num: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub create_at: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub update_at: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub status: Option<u32>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub contract_id: Option<u64>,
 }
 
 #[derive(Deserialize, Serialize, TypedBuilder)]
 pub struct CommitmentSpentForChainRequest {
     pub chain_id: u64,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub start_block: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub end_block: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub where_filter: Option<CommitmentSpentFilter>,
 }
 
@@ -41,11 +42,11 @@ pub struct CommitmentSpentForChainRequest {
 pub struct CommitmentSpentForContractRequest {
     pub chain_id: u64,
     pub address: String,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub start_block: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub end_block: Option<u64>,
-    #[builder(setter(strip_option), default=None)]
+    #[builder(setter(strip_option), default = None)]
     pub where_filter: Option<CommitmentSpentFilter>,
 }
 
@@ -63,4 +64,37 @@ pub struct CommitmentSpentResponse {
     pub update_at: u64,
     pub status: u32,
     pub contract_id: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
+pub struct DataLoaderRequest {
+    pub contract_address: String,
+    #[builder(setter(strip_option), default = None)]
+    pub start_block: Option<u64>,
+    #[builder(setter(strip_option), default = None)]
+    pub end_block: Option<u64>,
+}
+
+#[derive(Debug, Serialize, Deserialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
+pub struct DataLoaderResponse {
+    pub commitment_responses: Vec<CommitmentForDataLoaderResponse>,
+    pub nullifier_responses: Vec<NullifierForDataLoaderResponse>,
+}
+
+#[derive(Debug, Serialize, Deserialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
+pub struct NullifierForDataLoaderResponse {
+    pub contract_address: String,
+    pub current_sync_block_num: u64,
+    pub nullifiers: Vec<NullifierForDataLoader>,
+}
+
+#[derive(Debug, Serialize, Deserialize, TypedBuilder)]
+#[serde(rename_all = "camelCase")]
+pub struct NullifierForDataLoader {
+    pub nullifier: String,
+    pub block_number: u64,
+    pub transaction_hash: String,
 }
