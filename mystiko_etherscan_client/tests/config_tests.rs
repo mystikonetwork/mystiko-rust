d3504ed8f0f135acba2f59cdaf8af06ec4e1833a
Fitch Li <fitch@mystiko.network>
2023-09-01 11:48:20 +0800

    add base and arbitrum support for etherscan fetcher

--- mystiko_etherscan_client/tests/config_tests.rs
+++ mystiko_etherscan_client/tests/config_tests.rs
@@ -10,12 +10,16 @@ fn test_get_default_base_url() {
     assert!(get_default_base_url(43114u64).is_ok());
     assert!(get_default_base_url(250u64).is_ok());
     assert!(get_default_base_url(1284u64).is_ok());
+    assert!(get_default_base_url(8453u64).is_ok());
+    assert!(get_default_base_url(42161u64).is_ok());
     assert!(get_default_base_url(5u64).is_ok());
     assert!(get_default_base_url(97u64).is_ok());
     assert!(get_default_base_url(80001u64).is_ok());
     assert!(get_default_base_url(43113u64).is_ok());
     assert!(get_default_base_url(4002u64).is_ok());
     assert!(get_default_base_url(1287u64).is_ok());
+    assert!(get_default_base_url(84531u64).is_ok());
+    assert!(get_default_base_url(421613u64).is_ok());
     let result = get_default_base_url(0);
     assert!(result.is_err());
     let error = result.unwrap_err();
