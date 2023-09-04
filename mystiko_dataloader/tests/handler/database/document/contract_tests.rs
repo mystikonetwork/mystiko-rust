e70b49f6b358c075dec2f8cd3ecf80310568a461
Gary <jl81099173@gmail.com>
2023-09-04 19:19:38 +0800

    add tests

--- mystiko_dataloader/tests/handler/database/document/contract_tests.rs
+++ mystiko_dataloader/tests/handler/database/document/contract_tests.rs
@@ -1,1 +1,47 @@
+use mystiko_config::wrapper::mystiko::MystikoConfig;
+use mystiko_dataloader::handler::document::{Contract, DatabaseContract};
+use std::sync::Arc;
 
+#[test]
+fn test_contract() {
+    assert_eq!(Contract::column_chain_id(), "chain_id");
+    assert_eq!(Contract::column_contract_address(), "contract_address");
+    assert_eq!(Contract::column_loaded_block(), "loaded_block");
+    let mut contract = Contract::builder()
+        .chain_id(1u64)
+        .contract_address("address1".to_string())
+        .loaded_block(1000u64)
+        .build();
+    assert_eq!(contract.get_chain_id(), 1_u64);
+    assert_eq!(contract.get_contract_address(), "address1");
+    assert_eq!(contract.get_loaded_block(), 1000u64);
+    contract.update_loaded_block(2000u64);
+    assert_eq!(contract.get_chain_id(), 1u64);
+    assert_eq!(contract.get_contract_address(), "address1");
+    assert_eq!(contract.get_loaded_block(), 2000u64);
+}
+
+#[tokio::test]
+async fn test_from_config() {
+    let config = Arc::new(
+        MystikoConfig::from_json_file("./tests/files/handler/config.json")
+            .await
+            .unwrap(),
+    );
+    let contract_result =
+        Contract::from_config(Arc::clone(&config), 1u64, "0x7F88F2A3Cf18E96844E14CaE59EC97B908734C01");
+    assert!(contract_result.is_ok());
+    let contract = contract_result.unwrap();
+    assert_eq!(
+        contract.contract_address,
+        "0x7F88F2A3Cf18E96844E14CaE59EC97B908734C01".to_string()
+    );
+    assert_eq!(contract.chain_id, 1u64);
+    assert_eq!(contract.loaded_block, 16690439);
+    let contract_result = Contract::from_config(
+        Arc::clone(&config),
+        123456u64,
+        "0x7F88F2A3Cf18E96844E14CaE59EC97B908734C01",
+    );
+    assert!(contract_result.is_err());
+}
--- mystiko_dataloader/tests/handler/database/utils_tests.rs
+++ mystiko_dataloader/tests/handler/database/utils_tests.rs
@@ -1,7 +1,106 @@
-use mystiko_dataloader::handler::merge_commitments;
-use mystiko_protos::data::v1::{Commitment, CommitmentStatus};
+use mystiko_dataloader::handler::{dedup_commitments, dedup_nullifiers, merge_commitments};
+use mystiko_protos::data::v1::{Commitment, CommitmentStatus, Nullifier};
 use mystiko_utils::convert::biguint_to_bytes;
 use num_bigint::BigUint;
+
+#[test]
+fn test_dedup_commitments() {
+    let commitments: Vec<Commitment> = vec![
+        Commitment::builder()
+            .commitment_hash(biguint_to_bytes(&BigUint::from(100000u64)))
+            .block_number(100u64)
+            .status(CommitmentStatus::Queued as i32)
+            .leaf_index(10u64)
+            .rollup_fee(biguint_to_bytes(&BigUint::from(100u64)))
+            .encrypted_note(vec![1u8, 2u8, 3u8])
+            .queued_transaction_hash(vec![4u8, 5u8, 6u8])
+            .src_chain_transaction_hash(vec![7u8, 8u8, 9u8])
+            .build(),
+        Commitment::builder()
+            .commitment_hash(biguint_to_bytes(&BigUint::from(100000u64)))
+            .block_number(200u64)
+            .status(CommitmentStatus::Included as i32)
+            .included_block_number(200u64)
+            .included_transaction_hash(vec![7u8, 8u8, 9u8])
+            .build(),
+        Commitment::builder()
+            .commitment_hash(biguint_to_bytes(&BigUint::from(200000u64)))
+            .block_number(300u64)
+            .status(CommitmentStatus::Unspecified as i32)
+            .included_block_number(200u64)
+            .included_transaction_hash(vec![7u8, 8u8, 9u8])
+            .build(),
+        Commitment::builder()
+            .commitment_hash(biguint_to_bytes(&BigUint::from(300000u64)))
+            .block_number(250u64)
+            .status(CommitmentStatus::SrcSucceeded as i32)
+            .src_chain_block_number(250u64)
+            .src_chain_transaction_hash(vec![7u8, 8u8, 9u8])
+            .build(),
+    ];
+    let deduped_commitments: Vec<Commitment> = dedup_commitments(commitments);
+    assert_eq!(deduped_commitments.len(), 3);
+    assert_eq!(deduped_commitments[0].status, CommitmentStatus::Included as i32);
+    assert_eq!(deduped_commitments[0].included_block_number, Some(200u64));
+    assert_eq!(deduped_commitments[0].block_number, 100u64);
+    assert_eq!(
+        deduped_commitments[0].commitment_hash,
+        biguint_to_bytes(&BigUint::from(100000u64))
+    );
+    assert_eq!(
+        deduped_commitments[0].included_transaction_hash,
+        Some(vec![7u8, 8u8, 9u8])
+    );
+    assert_eq!(deduped_commitments[1].block_number, 250u64);
+    assert_eq!(
+        deduped_commitments[1].commitment_hash,
+        biguint_to_bytes(&BigUint::from(300000u64))
+    );
+    assert_eq!(deduped_commitments[1].status, CommitmentStatus::SrcSucceeded as i32);
+    assert_eq!(deduped_commitments[1].src_chain_block_number, Some(250u64));
+    assert_eq!(
+        deduped_commitments[1].src_chain_transaction_hash,
+        Some(vec![7u8, 8u8, 9u8])
+    );
+    assert_eq!(deduped_commitments[2].block_number, 300u64);
+    assert_eq!(
+        deduped_commitments[2].commitment_hash,
+        biguint_to_bytes(&BigUint::from(200000u64))
+    );
+    assert_eq!(deduped_commitments[2].status, CommitmentStatus::Unspecified as i32);
+}
+
+#[test]
+fn test_dedup_nullifiers() {
+    let deduped_nullifiers: Vec<Nullifier> = dedup_nullifiers(vec![
+        Nullifier::builder()
+            .nullifier(vec![1u8, 2u8, 3u8])
+            .block_number(100u64)
+            .transaction_hash(vec![1u8, 2u8, 3u8])
+            .build(),
+        Nullifier::builder()
+            .nullifier(vec![1u8, 2u8, 3u8])
+            .block_number(200u64)
+            .transaction_hash(vec![11u8, 22u8, 33u8])
+            .build(),
+        Nullifier::builder()
+            .nullifier(vec![4u8, 5u8, 6u8])
+            .block_number(500u64)
+            .transaction_hash(vec![1u8, 2u8, 3u8])
+            .build(),
+        Nullifier::builder()
+            .nullifier(vec![7u8, 8u8, 9u8])
+            .block_number(300u64)
+            .transaction_hash(vec![1u8, 2u8, 3u8])
+            .build(),
+    ]);
+    assert_eq!(deduped_nullifiers.len(), 3);
+    assert_eq!(deduped_nullifiers[0].block_number, 200u64);
+    assert_eq!(deduped_nullifiers[0].nullifier, vec![1u8, 2u8, 3u8]);
+    assert_eq!(deduped_nullifiers[0].transaction_hash, vec![11u8, 22u8, 33u8]);
+    assert_eq!(deduped_nullifiers[1].block_number, 300u64);
+    assert_eq!(deduped_nullifiers[2].block_number, 500u64);
+}
 
 #[test]
 fn test_merge_commitments() {
