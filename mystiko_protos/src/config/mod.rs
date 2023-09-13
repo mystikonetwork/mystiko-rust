8a3c2eaf77764f77d3fa8c127a7c3ebd37aeb5c0
Fitch Li <fitch@mystiko.network>
2023-09-13 22:09:33 +0800

    fix missing mod re-export of some protos modules (#152)

--- mystiko_protos/src/config/mod.rs
+++ mystiko_protos/src/config/mod.rs
@@ -1,2 +1,7 @@
+#[cfg(feature = "mystiko-config-bridge-v1")]
+pub mod bridge;
+#[cfg(feature = "mystiko-config-contract-v1")]
+pub mod contract;
+
 #[cfg(feature = "mystiko-config-v1")]
 pub mod v1;
