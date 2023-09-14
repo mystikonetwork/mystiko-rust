a58da913520e7a40f0ec452a9db536203bfcf83f
ericlurker <96100006+ericlurker@users.noreply.github.com>
2023-09-14 13:30:35 +0800

    add more tests

--- mystiko_dataloader/tests/loader/fetcher/chain_fetcher_with_handler_tests.rs
+++ mystiko_dataloader/tests/loader/fetcher/chain_fetcher_with_handler_tests.rs
@@ -6,7 +6,7 @@ use rand::seq::SliceRandom;
 use std::collections::HashSet;
 
 #[tokio::test]
-async fn test_loader_many_fetcher_loaded_block_all_success() {
+async fn test_loader_many_fetcher_all_success() {
     let chain_id = 1_u64;
     let (cfg, loader, fetchers, _, handler) = create_loader(chain_id, 4, 1, false).await;
     let contract_address1 = "0x932f3DD5b6C0F5fe1aEc31Cb38B7a57d01496411";
@@ -45,4 +45,74 @@ async fn test_loader_many_fetcher_loaded_block_all_success() {
         ));
         assert!(result.is_ok());
     }
+}
+
+#[tokio::test]
+async fn test_loader_many_fetcher_some_fetcher_error() {
+    let chain_id = 1_u64;
+    let (cfg, loader, fetchers, _, handler) = create_loader(chain_id, 4, 1, false).await;
+    let contract_address1 = "0x932f3DD5b6C0F5fe1aEc31Cb38B7a57d01496411";
+    let mut contracts = HashSet::new();
+    contracts.insert(contract_address1);
+    handler.set_contracts(chain_id, contracts, cfg.clone()).await;
+
+    let start_block = cfg.find_chain(chain_id).unwrap().start_block() + 1;
+    let target_block = start_block + 1000;
+
+    let mut contract_data = vec![ContractData::builder()
+        .address(contract_address1)
+        .start_block(start_block)
+        .end_block(target_block)
+        .build()];
+
+    for skip in 0..3 {
+        let mut numbers: Vec<usize> = (0..4).collect();
+        numbers.shuffle(&mut rand::thread_rng());
+        for i in numbers {
+            let loaded_block = target_block - i as u64;
+            contract_data[0].end_block = loaded_block;
+            let fetcher_result = ChainData::builder()
+                .chain_id(chain_id)
+                .contracts_data(contract_data.clone())
+                .build();
+            fetchers[i].set_loaded_block(Some(loaded_block)).await;
+            if i <= skip {
+                fetchers[i].set_fetch_error_result().await;
+            } else {
+                fetchers[i].set_fetch_result(fetcher_result.clone()).await;
+            }
+        }
+
+        let result = loader.load(None).await;
+        contract_data[0].end_block = target_block - skip as u64 - 1;
+        assert!(contract_data_partial_eq(
+            &handler.drain_data().await,
+            &[contract_data.clone()].concat()
+        ));
+        assert!(result.is_ok());
+    }
+}
+
+#[tokio::test]
+async fn test_loader_many_fetcher_all_error() {
+    let chain_id = 1_u64;
+    let (cfg, loader, fetchers, _, handler) = create_loader(chain_id, 4, 1, false).await;
+    let contract_address1 = "0x932f3DD5b6C0F5fe1aEc31Cb38B7a57d01496411";
+    let mut contracts = HashSet::new();
+    contracts.insert(contract_address1);
+    handler.set_contracts(chain_id, contracts, cfg.clone()).await;
+
+    let start_block = cfg.find_chain(chain_id).unwrap().start_block() + 1;
+    let target_block = start_block + 1000;
+
+    let mut numbers: Vec<usize> = (0..4).collect();
+    numbers.shuffle(&mut rand::thread_rng());
+    for i in numbers {
+        let loaded_block = target_block - i as u64;
+        fetchers[i].set_loaded_block(Some(loaded_block)).await;
+        fetchers[i].set_fetch_error_result().await;
+    }
+
+    let result = loader.load(None).await;
+    assert!(result.is_err());
 }
