d4ae61c0313b9261f29f6de30b2550ea84c6d5a7
moomarco <iamwumaixing@gmail.com>
2023-09-14 19:16:07 +0800

    add unit tests

--- mystiko_config/tests/wrapper/bridge/bridge_tests.rs
+++ mystiko_config/tests/wrapper/bridge/bridge_tests.rs
@@ -49,3 +49,53 @@ async fn test_create() {
     assert_eq!(bridge_config4.bridge_type(), &BridgeType::Poly);
     assert_eq!(bridge_config5.bridge_type(), &BridgeType::Tbridge);
 }
+
+#[tokio::test]
+async fn test_to_proto() {
+    let raw_axelar_config = create_raw_from_file::<RawAxelarBridgeConfig>(AXELAR_CONFIG_FILE)
+        .await
+        .unwrap();
+    let raw_celer_config = create_raw_from_file::<RawCelerBridgeConfig>(CELER_CONFIG_FILE)
+        .await
+        .unwrap();
+    let raw_layer_zero_config = create_raw_from_file::<RawLayerZeroBridgeConfig>(LAYER_ZERO_CONFIG_FILE)
+        .await
+        .unwrap();
+    let raw_poly_config = create_raw_from_file::<RawPolyBridgeConfig>(POLY_CONFIG_FILE)
+        .await
+        .unwrap();
+    let raw_tbridge_config = create_raw_from_file::<RawTBridgeConfig>(TBRIDGE_CONFIG_FILE)
+        .await
+        .unwrap();
+    let bridge_config1 = BridgeConfig::new(Arc::new(RawBridgeConfig::Axelar(Arc::new(raw_axelar_config)))).to_proto();
+    let bridge_config2 = BridgeConfig::new(Arc::new(RawBridgeConfig::Celer(Arc::new(raw_celer_config)))).to_proto();
+    let bridge_config3 =
+        BridgeConfig::new(Arc::new(RawBridgeConfig::LayerZero(Arc::new(raw_layer_zero_config)))).to_proto();
+    let bridge_config4 = BridgeConfig::new(Arc::new(RawBridgeConfig::Poly(Arc::new(raw_poly_config)))).to_proto();
+    let bridge_config5 = BridgeConfig::new(Arc::new(RawBridgeConfig::Tbridge(Arc::new(raw_tbridge_config)))).to_proto();
+    assert_eq!(&bridge_config1.name, "Axelar Bridge");
+    assert_eq!(&bridge_config2.name, "Celer Bridge");
+    assert_eq!(&bridge_config3.name, "LayerZero Bridge");
+    assert_eq!(&bridge_config4.name, "Poly Bridge");
+    assert_eq!(&bridge_config5.name, "Mystiko Testnet Bridge");
+    assert_eq!(
+        bridge_config1.bridge_type(),
+        mystiko_protos::common::v1::BridgeType::Axelar
+    );
+    assert_eq!(
+        bridge_config2.bridge_type(),
+        mystiko_protos::common::v1::BridgeType::Celer
+    );
+    assert_eq!(
+        bridge_config3.bridge_type(),
+        mystiko_protos::common::v1::BridgeType::LayerZero
+    );
+    assert_eq!(
+        bridge_config4.bridge_type(),
+        mystiko_protos::common::v1::BridgeType::Poly
+    );
+    assert_eq!(
+        bridge_config5.bridge_type(),
+        mystiko_protos::common::v1::BridgeType::Tbridge
+    );
+}
