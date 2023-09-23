26c310f0460c7e6d20a643b9d30d0556869ed55e
Fitch Li <fitch@mystiko.network>
2023-09-23 11:11:13 +0800

    add status http server for mystiko_scheduler (#171)

--- mystiko_scheduler/src/error.rs
+++ mystiko_scheduler/src/error.rs
@@ -8,9 +8,24 @@ pub enum SchedulerError {
     SendStartSignalError,
     #[error("failed to send stop signal")]
     SendStopSignalError,
+    #[cfg(feature = "status")]
+    #[error("failed to send status server's start signal")]
+    SendStatusServerStartSignalError,
+    #[cfg(feature = "status")]
+    #[error("failed to send status server's stop signal")]
+    SendStatusServerStopSignalError,
     #[error(transparent)]
     JoinError(#[from] JoinError),
     #[cfg(feature = "signal")]
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+    #[cfg(feature = "status")]
+    #[error(transparent)]
+    AddrParseError(#[from] std::net::AddrParseError),
+    #[cfg(feature = "status")]
+    #[error(transparent)]
+    HttpError(#[from] http::Error),
+    #[cfg(feature = "status")]
+    #[error(transparent)]
+    HyperError(#[from] hyper::Error),
 }
--- mystiko_scheduler/tests/scheduler_tests.rs
+++ mystiko_scheduler/tests/scheduler_tests.rs
@@ -1,5 +1,11 @@
 use async_trait::async_trait;
-use mystiko_scheduler::{AbortPolicy, RetryPolicy, Scheduler, SchedulerTask, StartOptions};
+use hyper::body::Buf;
+use hyper::Body;
+use mime::Mime;
+use mystiko_scheduler::{
+    AbortPolicy, RetryPolicy, Scheduler, SchedulerOptions, SchedulerStatus, SchedulerTask, StartOptions,
+};
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::Mutex;
@@ -25,7 +31,11 @@ async fn test_start_and_stop() {
 async fn test_start_multiple_times() {
     setup();
     let task = Arc::<CounterTask>::default();
-    let scheduler = Scheduler::new(task);
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task)
+        .status_server_port(23212u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     scheduler
         .start(
             None,
@@ -50,7 +60,11 @@ async fn test_start_multiple_times() {
 async fn test_stop_before_start() {
     setup();
     let task = Arc::<CounterTask>::default();
-    let scheduler = Scheduler::new(task);
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task)
+        .status_server_port(23213u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     scheduler.join().await.unwrap();
     scheduler.stop().await.unwrap();
 }
@@ -59,7 +73,11 @@ async fn test_stop_before_start() {
 async fn test_task_timeout() {
     setup();
     let task = Arc::new(CounterTask::builder().sleep_ms(10000u64).build());
-    let scheduler = Scheduler::new(task.clone());
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23214u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     let options = StartOptions::<anyhow::Error>::builder()
         .interval_ms(2u64)
         .task_timeout_ms(1u64)
@@ -75,7 +93,11 @@ async fn test_task_timeout() {
 async fn test_task_timeout_with_abort() {
     setup();
     let task = Arc::new(CounterTask::builder().sleep_ms(10000u64).build());
-    let scheduler = Scheduler::new(task.clone());
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23215u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     let options = StartOptions::<anyhow::Error>::builder()
         .interval_ms(2u64)
         .task_timeout_ms(1u64)
@@ -90,7 +112,11 @@ async fn test_task_timeout_with_abort() {
 async fn test_task_timeout_with_retry() {
     setup();
     let task = Arc::new(CounterTask::builder().sleep_ms(10000u64).build());
-    let scheduler = Scheduler::new(task.clone());
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23216u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     let options = StartOptions::<anyhow::Error>::builder()
         .interval_ms(2u64)
         .task_timeout_ms(1u64)
@@ -106,7 +132,11 @@ async fn test_task_timeout_with_retry() {
 async fn test_retry_policy() {
     setup();
     let task = Arc::<CounterTask>::default();
-    let scheduler = Scheduler::new(task.clone());
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23217u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     let options = StartOptions::<anyhow::Error>::builder()
         .interval_ms(2u64)
         .max_retry_times(2u32)
@@ -142,7 +172,11 @@ async fn test_retry_policy() {
 async fn test_abort_policy() {
     setup();
     let task = Arc::new(CounterTask::builder().abort_on_count(4u32).build());
-    let scheduler = Scheduler::new(task.clone());
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23218u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
     let options = StartOptions::<anyhow::Error>::builder()
         .interval_ms(2u64)
         .abort_policy(Arc::new(
@@ -153,6 +187,77 @@ async fn test_abort_policy() {
     scheduler.join().await.unwrap();
     assert!(!scheduler.started().await);
     assert_eq!(*task.counter.lock().await, 4u32);
+}
+
+#[tokio::test]
+async fn test_http_status() {
+    setup();
+    let task = Arc::new(CounterTask::builder().abort_on_count(4u32).build());
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task)
+        .status_server_port(23219u16)
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
+    let client = hyper::Client::new();
+    let status_uri: http::Uri = "http://localhost:23219/status".parse().unwrap();
+    let non_existing_uri: http::Uri = "http://localhost:23219/non_existing".parse().unwrap();
+    assert!(client.get(status_uri.clone()).await.is_err());
+    assert!(client.get(non_existing_uri.clone()).await.is_err());
+
+    let options = StartOptions::<anyhow::Error>::builder().interval_ms(1000u64).build();
+    scheduler.start(None, options).await.unwrap();
+
+    let status_response = client.get(status_uri.clone()).await.unwrap();
+    assert!(status_response.status().is_success());
+
+    let non_existing_response = client.get(non_existing_uri).await.unwrap();
+    assert_eq!(non_existing_response.status(), http::StatusCode::NOT_FOUND);
+
+    scheduler.stop().await.unwrap();
+    assert!(client.get(status_uri).await.is_err());
+}
+
+#[tokio::test]
+async fn test_http_custom_status() {
+    setup();
+    let task = Arc::new(CounterTask::builder().abort_on_count(4u32).build());
+    let expected_status = CounterStatus::builder().count(1234u32).build();
+    let status_getter = CounterStatusGetter::builder().status(expected_status.clone()).build();
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23220u16)
+        .status_getter(Arc::new(Box::new(status_getter) as Box<dyn SchedulerStatus>))
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
+    let options = StartOptions::<anyhow::Error>::builder().interval_ms(1000u64).build();
+    scheduler.start(None, options).await.unwrap();
+
+    let client = hyper::Client::new();
+    let status_uri: http::Uri = "http://localhost:23220/status".parse().unwrap();
+    let status_response = client.get(status_uri.clone()).await.unwrap();
+    let status_body = hyper::body::aggregate(status_response).await.unwrap();
+    let status = serde_json::from_reader::<_, CounterStatus>(status_body.reader()).unwrap();
+    assert_eq!(status, expected_status);
+}
+
+#[tokio::test]
+async fn test_http_custom_status_with_error() {
+    setup();
+    let task = Arc::new(CounterTask::builder().abort_on_count(4u32).build());
+    let status_getter = CounterStatusGetter::builder().error_on_status(true).build();
+    let scheduler_options = SchedulerOptions::<Option<_>, CounterTask>::builder()
+        .task(task.clone())
+        .status_server_port(23221u16)
+        .status_getter(Arc::new(Box::new(status_getter) as Box<dyn SchedulerStatus>))
+        .build();
+    let scheduler = Scheduler::new(scheduler_options);
+    let options = StartOptions::<anyhow::Error>::builder().interval_ms(1000u64).build();
+    scheduler.start(None, options).await.unwrap();
+
+    let client = hyper::Client::new();
+    let status_uri: http::Uri = "http://localhost:23221/status".parse().unwrap();
+    let status_response = client.get(status_uri.clone()).await.unwrap();
+    assert!(status_response.status().is_server_error());
 }
 
 fn setup() {
@@ -175,6 +280,19 @@ struct CounterAbortPolicy;
 #[derive(Debug, Clone, Default)]
 struct CounterRetryPolicy;
 
+#[derive(Debug, Clone, Default, TypedBuilder)]
+#[builder(field_defaults(default, setter(into)))]
+struct CounterStatusGetter {
+    pub(crate) status: CounterStatus,
+    pub(crate) error_on_status: bool,
+}
+
+#[derive(Debug, Clone, Default, Eq, PartialEq, Serialize, Deserialize, TypedBuilder)]
+#[builder(field_defaults(default, setter(into)))]
+struct CounterStatus {
+    count: u32,
+}
+
 #[async_trait]
 impl SchedulerTask<Option<anyhow::Error>> for CounterTask {
     type Error = anyhow::Error;
@@ -193,6 +311,17 @@ impl SchedulerTask<Option<anyhow::Error>> for CounterTask {
             tokio::time::sleep(Duration::from_millis(sleep_ms)).await;
         }
         Ok(())
+    }
+}
+
+#[async_trait]
+impl SchedulerStatus for CounterStatusGetter {
+    async fn status(&self) -> anyhow::Result<(Mime, Body)> {
+        if self.error_on_status {
+            return Err(anyhow::anyhow!("query status error"));
+        }
+        let counter_status = serde_json::to_string(&self.status)?;
+        Ok((mime::APPLICATION_JSON, Body::from(counter_status)))
     }
 }
 
