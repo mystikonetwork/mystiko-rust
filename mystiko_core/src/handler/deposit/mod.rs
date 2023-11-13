d46b49909ea230b619e5d57125dbb73990cd0e47
Fitch Li <fitch@mystiko.network>
2023-11-13 18:09:47 +0800

    add 'static bound to deposit handler methods (#230)

--- mystiko_core/src/handler/deposit/mod.rs
+++ mystiko_core/src/handler/deposit/mod.rs
@@ -26,19 +26,19 @@ pub trait DepositHandler<D, QO, Q, C, DS, S>: Send + Sync {
 
     async fn find<Filter>(&self, filter: Filter) -> Result<Vec<D>, Self::Error>
     where
-        Filter: Into<QueryFilter> + Send + Sync;
+        Filter: Into<QueryFilter> + Send + Sync + 'static;
 
     async fn find_all(&self) -> Result<Vec<D>, Self::Error>;
 
     async fn find_one<Filter>(&self, filter: Filter) -> Result<Option<D>, Self::Error>
     where
-        Filter: Into<QueryFilter> + Send + Sync;
+        Filter: Into<QueryFilter> + Send + Sync + 'static;
 
     async fn find_by_id(&self, id: String) -> Result<Option<D>, Self::Error>;
 
     async fn count<Filter>(&self, filter: Filter) -> Result<u64, Self::Error>
     where
-        Filter: Into<QueryFilter> + Send + Sync;
+        Filter: Into<QueryFilter> + Send + Sync + 'static;
 
     async fn count_all(&self) -> Result<u64, Self::Error>;
 
@@ -48,12 +48,12 @@ pub trait DepositHandler<D, QO, Q, C, DS, S>: Send + Sync {
 
     async fn update_by_filter<Filter, Values>(&self, column_values: Values, filter: Filter) -> Result<(), Self::Error>
     where
-        Filter: Into<QueryFilter> + Send + Sync,
-        Values: Into<ColumnValues> + Send + Sync;
+        Filter: Into<QueryFilter> + Send + Sync + 'static,
+        Values: Into<ColumnValues> + Send + Sync + 'static;
 
     async fn update_all<Values>(&self, column_values: Values) -> Result<(), Self::Error>
     where
-        Values: Into<ColumnValues> + Send + Sync;
+        Values: Into<ColumnValues> + Send + Sync + 'static;
 
     async fn delete(&self, deposit: D) -> Result<(), Self::Error>;
 
@@ -61,7 +61,7 @@ pub trait DepositHandler<D, QO, Q, C, DS, S>: Send + Sync {
 
     async fn delete_by_filter<Filter>(&self, filter: Filter) -> Result<(), Self::Error>
     where
-        Filter: Into<QueryFilter> + Send + Sync;
+        Filter: Into<QueryFilter> + Send + Sync + 'static;
 
     async fn delete_all(&self) -> Result<(), Self::Error>;
 }
