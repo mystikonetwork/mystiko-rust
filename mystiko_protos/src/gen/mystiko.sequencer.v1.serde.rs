97e6900fc2ff807ecf4b44b17ff7f94dd6763733
Fitch Li <fitch@mystiko.network>
2024-04-15 13:03:22 +0800

    add get_commitment_hashes method to sequencer client

--- mystiko_protos/src/gen/mystiko.sequencer.v1.serde.rs
+++ mystiko_protos/src/gen/mystiko.sequencer.v1.serde.rs
@@ -1072,6 +1072,354 @@ impl<'de> serde::Deserialize<'de> for FetchContractResponse {
         deserializer.deserialize_struct("mystiko.sequencer.v1.FetchContractResponse", FIELDS, GeneratedVisitor)
     }
 }
+impl serde::Serialize for GetCommitmentHashesRequest {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.chain_id != 0 {
+            len += 1;
+        }
+        if !self.contract_address.is_empty() {
+            len += 1;
+        }
+        if self.from_leaf_index.is_some() {
+            len += 1;
+        }
+        if self.to_leaf_index.is_some() {
+            len += 1;
+        }
+        if self.status.is_some() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("mystiko.sequencer.v1.GetCommitmentHashesRequest", len)?;
+        if self.chain_id != 0 {
+            struct_ser.serialize_field("chainId", ToString::to_string(&self.chain_id).as_str())?;
+        }
+        if !self.contract_address.is_empty() {
+            struct_ser.serialize_field("contractAddress", pbjson::private::base64::encode(&self.contract_address).as_str())?;
+        }
+        if let Some(v) = self.from_leaf_index.as_ref() {
+            struct_ser.serialize_field("fromLeafIndex", ToString::to_string(&v).as_str())?;
+        }
+        if let Some(v) = self.to_leaf_index.as_ref() {
+            struct_ser.serialize_field("toLeafIndex", ToString::to_string(&v).as_str())?;
+        }
+        if let Some(v) = self.status.as_ref() {
+            let v = super::super::data::v1::CommitmentStatus::from_i32(*v)
+                .ok_or_else(|| serde::ser::Error::custom(format!("Invalid variant {}", *v)))?;
+            struct_ser.serialize_field("status", &v)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for GetCommitmentHashesRequest {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "chain_id",
+            "chainId",
+            "contract_address",
+            "contractAddress",
+            "from_leaf_index",
+            "fromLeafIndex",
+            "to_leaf_index",
+            "toLeafIndex",
+            "status",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            ChainId,
+            ContractAddress,
+            FromLeafIndex,
+            ToLeafIndex,
+            Status,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "chainId" | "chain_id" => Ok(GeneratedField::ChainId),
+                            "contractAddress" | "contract_address" => Ok(GeneratedField::ContractAddress),
+                            "fromLeafIndex" | "from_leaf_index" => Ok(GeneratedField::FromLeafIndex),
+                            "toLeafIndex" | "to_leaf_index" => Ok(GeneratedField::ToLeafIndex),
+                            "status" => Ok(GeneratedField::Status),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = GetCommitmentHashesRequest;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct mystiko.sequencer.v1.GetCommitmentHashesRequest")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> std::result::Result<GetCommitmentHashesRequest, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut chain_id__ = None;
+                let mut contract_address__ = None;
+                let mut from_leaf_index__ = None;
+                let mut to_leaf_index__ = None;
+                let mut status__ = None;
+                while let Some(k) = map.next_key()? {
+                    match k {
+                        GeneratedField::ChainId => {
+                            if chain_id__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("chainId"));
+                            }
+                            chain_id__ = 
+                                Some(map.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::ContractAddress => {
+                            if contract_address__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("contractAddress"));
+                            }
+                            contract_address__ = 
+                                Some(map.next_value::<::pbjson::private::BytesDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::FromLeafIndex => {
+                            if from_leaf_index__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("fromLeafIndex"));
+                            }
+                            from_leaf_index__ = 
+                                map.next_value::<::std::option::Option<::pbjson::private::NumberDeserialize<_>>>()?.map(|x| x.0)
+                            ;
+                        }
+                        GeneratedField::ToLeafIndex => {
+                            if to_leaf_index__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("toLeafIndex"));
+                            }
+                            to_leaf_index__ = 
+                                map.next_value::<::std::option::Option<::pbjson::private::NumberDeserialize<_>>>()?.map(|x| x.0)
+                            ;
+                        }
+                        GeneratedField::Status => {
+                            if status__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("status"));
+                            }
+                            status__ = map.next_value::<::std::option::Option<super::super::data::v1::CommitmentStatus>>()?.map(|x| x as i32);
+                        }
+                    }
+                }
+                Ok(GetCommitmentHashesRequest {
+                    chain_id: chain_id__.unwrap_or_default(),
+                    contract_address: contract_address__.unwrap_or_default(),
+                    from_leaf_index: from_leaf_index__,
+                    to_leaf_index: to_leaf_index__,
+                    status: status__,
+                })
+            }
+        }
+        deserializer.deserialize_struct("mystiko.sequencer.v1.GetCommitmentHashesRequest", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for GetCommitmentHashesResponse {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.chain_id != 0 {
+            len += 1;
+        }
+        if !self.contract_address.is_empty() {
+            len += 1;
+        }
+        if self.from_leaf_index != 0 {
+            len += 1;
+        }
+        if self.to_leaf_index != 0 {
+            len += 1;
+        }
+        if !self.commitment_hashes.is_empty() {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("mystiko.sequencer.v1.GetCommitmentHashesResponse", len)?;
+        if self.chain_id != 0 {
+            struct_ser.serialize_field("chainId", ToString::to_string(&self.chain_id).as_str())?;
+        }
+        if !self.contract_address.is_empty() {
+            struct_ser.serialize_field("contractAddress", pbjson::private::base64::encode(&self.contract_address).as_str())?;
+        }
+        if self.from_leaf_index != 0 {
+            struct_ser.serialize_field("fromLeafIndex", ToString::to_string(&self.from_leaf_index).as_str())?;
+        }
+        if self.to_leaf_index != 0 {
+            struct_ser.serialize_field("toLeafIndex", ToString::to_string(&self.to_leaf_index).as_str())?;
+        }
+        if !self.commitment_hashes.is_empty() {
+            struct_ser.serialize_field("commitmentHashes", &self.commitment_hashes.iter().map(pbjson::private::base64::encode).collect::<Vec<_>>())?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for GetCommitmentHashesResponse {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "chain_id",
+            "chainId",
+            "contract_address",
+            "contractAddress",
+            "from_leaf_index",
+            "fromLeafIndex",
+            "to_leaf_index",
+            "toLeafIndex",
+            "commitment_hashes",
+            "commitmentHashes",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            ChainId,
+            ContractAddress,
+            FromLeafIndex,
+            ToLeafIndex,
+            CommitmentHashes,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "chainId" | "chain_id" => Ok(GeneratedField::ChainId),
+                            "contractAddress" | "contract_address" => Ok(GeneratedField::ContractAddress),
+                            "fromLeafIndex" | "from_leaf_index" => Ok(GeneratedField::FromLeafIndex),
+                            "toLeafIndex" | "to_leaf_index" => Ok(GeneratedField::ToLeafIndex),
+                            "commitmentHashes" | "commitment_hashes" => Ok(GeneratedField::CommitmentHashes),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = GetCommitmentHashesResponse;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct mystiko.sequencer.v1.GetCommitmentHashesResponse")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> std::result::Result<GetCommitmentHashesResponse, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut chain_id__ = None;
+                let mut contract_address__ = None;
+                let mut from_leaf_index__ = None;
+                let mut to_leaf_index__ = None;
+                let mut commitment_hashes__ = None;
+                while let Some(k) = map.next_key()? {
+                    match k {
+                        GeneratedField::ChainId => {
+                            if chain_id__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("chainId"));
+                            }
+                            chain_id__ = 
+                                Some(map.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::ContractAddress => {
+                            if contract_address__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("contractAddress"));
+                            }
+                            contract_address__ = 
+                                Some(map.next_value::<::pbjson::private::BytesDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::FromLeafIndex => {
+                            if from_leaf_index__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("fromLeafIndex"));
+                            }
+                            from_leaf_index__ = 
+                                Some(map.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::ToLeafIndex => {
+                            if to_leaf_index__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("toLeafIndex"));
+                            }
+                            to_leaf_index__ = 
+                                Some(map.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                        GeneratedField::CommitmentHashes => {
+                            if commitment_hashes__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("commitmentHashes"));
+                            }
+                            commitment_hashes__ = 
+                                Some(map.next_value::<Vec<::pbjson::private::BytesDeserialize<_>>>()?
+                                    .into_iter().map(|x| x.0).collect())
+                            ;
+                        }
+                    }
+                }
+                Ok(GetCommitmentHashesResponse {
+                    chain_id: chain_id__.unwrap_or_default(),
+                    contract_address: contract_address__.unwrap_or_default(),
+                    from_leaf_index: from_leaf_index__.unwrap_or_default(),
+                    to_leaf_index: to_leaf_index__.unwrap_or_default(),
+                    commitment_hashes: commitment_hashes__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("mystiko.sequencer.v1.GetCommitmentHashesResponse", FIELDS, GeneratedVisitor)
+    }
+}
 impl serde::Serialize for GetCommitmentsByTxHashRequest {
     #[allow(deprecated)]
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
